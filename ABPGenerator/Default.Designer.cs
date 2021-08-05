f8fa844c4590c1380c67e5d5458dd1564d90b5e6
vanteking <982138078@qq.com>
2021-08-05 17:05:12 +0800

    v2.0.0

--- ABPGenerator/Default.Designer.cs
+++ ABPGenerator/Default.Designer.cs
@@ -37,15 +37,18 @@
             this.cmbDb = new System.Windows.Forms.ComboBox();
             this.txtServerName = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtCodePath = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.txtUsername = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.txtPwd = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.txtDatatName = new System.Windows.Forms.TextBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.txtPermissionName = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cebView = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmbTheme = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -59,9 +62,9 @@
             // 
             // btnConnection
             // 
-            this.btnConnection.Location = new System.Drawing.Point(434, 111);
+            this.btnConnection.Location = new System.Drawing.Point(635, 110);
             this.btnConnection.Name = "btnConnection";
-            this.btnConnection.Size = new System.Drawing.Size(109, 30);
+            this.btnConnection.Size = new System.Drawing.Size(109, 41);
             this.btnConnection.TabIndex = 11;
             this.btnConnection.Text = "连接数据库";
             this.btnConnection.UseVisualStyleBackColor = true;
@@ -69,9 +72,9 @@
             // 
             // btnGenerator
             // 
-            this.btnGenerator.Location = new System.Drawing.Point(132, 352);
+            this.btnGenerator.Location = new System.Drawing.Point(635, 305);
             this.btnGenerator.Name = "btnGenerator";
-            this.btnGenerator.Size = new System.Drawing.Size(282, 30);
+            this.btnGenerator.Size = new System.Drawing.Size(109, 39);
             this.btnGenerator.TabIndex = 15;
             this.btnGenerator.Text = "生成代码";
             this.btnGenerator.UseVisualStyleBackColor = true;
@@ -80,7 +83,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(131, 175);
+            this.label2.Location = new System.Drawing.Point(323, 94);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 13;
@@ -98,10 +101,11 @@
             // cmbDb
             // 
             this.cmbDb.FormattingEnabled = true;
-            this.cmbDb.Location = new System.Drawing.Point(132, 203);
+            this.cmbDb.Location = new System.Drawing.Point(325, 122);
             this.cmbDb.Name = "cmbDb";
-            this.cmbDb.Size = new System.Drawing.Size(281, 20);
+            this.cmbDb.Size = new System.Drawing.Size(260, 20);
             this.cmbDb.TabIndex = 14;
+            this.cmbDb.SelectedIndexChanged += new System.EventHandler(this.cmbDb_SelectedIndexChanged);
             // 
             // txtServerName
             // 
@@ -119,59 +123,26 @@
             this.pictureBox1.TabIndex = 16;
             this.pictureBox1.TabStop = false;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(131, 257);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(173, 12);
-            this.label3.TabIndex = 18;
-            this.label3.Text = "选择代码保存路径（默认桌面）";
-            // 
-            // txtCodePath
-            // 
-            this.txtCodePath.Location = new System.Drawing.Point(133, 288);
-            this.txtCodePath.Name = "txtCodePath";
-            this.txtCodePath.Size = new System.Drawing.Size(184, 21);
-            this.txtCodePath.TabIndex = 17;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(335, 288);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(78, 23);
-            this.button1.TabIndex = 19;
-            this.button1.Text = "选择文件夹";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(305, 52);
+            this.txtUsername.Location = new System.Drawing.Point(325, 52);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(109, 21);
+            this.txtUsername.Size = new System.Drawing.Size(87, 21);
             this.txtUsername.TabIndex = 21;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(303, 24);
+            this.label5.Location = new System.Drawing.Point(323, 24);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(41, 12);
             this.label5.TabIndex = 20;
             this.label5.Text = "用户名";
             // 
-            // txtPwd
-            // 
-            this.txtPwd.Location = new System.Drawing.Point(434, 52);
-            this.txtPwd.Name = "txtPwd";
-            this.txtPwd.Size = new System.Drawing.Size(109, 21);
-            this.txtPwd.TabIndex = 23;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(432, 24);
+            this.label6.Location = new System.Drawing.Point(452, 24);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(29, 12);
             this.label6.TabIndex = 22;
@@ -179,34 +150,96 @@
             // 
             // txtDatatName
             // 
-            this.txtDatatName.Location = new System.Drawing.Point(132, 117);
+            this.txtDatatName.Location = new System.Drawing.Point(132, 121);
             this.txtDatatName.Name = "txtDatatName";
-            this.txtDatatName.Size = new System.Drawing.Size(282, 21);
+            this.txtDatatName.Size = new System.Drawing.Size(148, 21);
             this.txtDatatName.TabIndex = 25;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(130, 90);
+            this.label7.Location = new System.Drawing.Point(130, 94);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(65, 12);
             this.label7.TabIndex = 24;
             this.label7.Text = "数据库名称";
             // 
+            // txtPermissionName
+            // 
+            this.txtPermissionName.Location = new System.Drawing.Point(326, 237);
+            this.txtPermissionName.Name = "txtPermissionName";
+            this.txtPermissionName.Size = new System.Drawing.Size(258, 21);
+            this.txtPermissionName.TabIndex = 27;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(324, 209);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "权限名称";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Location = new System.Drawing.Point(454, 52);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(130, 21);
+            this.txtPwd.TabIndex = 23;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(131, 209);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(119, 12);
+            this.label9.TabIndex = 28;
+            this.label9.Text = "生成超管控制器/视图";
+            // 
+            // cebView
+            // 
+            this.cebView.AutoSize = true;
+            this.cebView.Location = new System.Drawing.Point(133, 239);
+            this.cebView.Name = "cebView";
+            this.cebView.Size = new System.Drawing.Size(36, 16);
+            this.cebView.TabIndex = 29;
+            this.cebView.Text = "是";
+            this.cebView.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 290);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 30;
+            this.label3.Text = "生成界面皮肤";
+            // 
+            // cmbTheme
+            // 
+            this.cmbTheme.FormattingEnabled = true;
+            this.cmbTheme.Location = new System.Drawing.Point(326, 325);
+            this.cmbTheme.Name = "cmbTheme";
+            this.cmbTheme.Size = new System.Drawing.Size(258, 20);
+            this.cmbTheme.TabIndex = 31;
+            // 
             // Default
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(604, 425);
+            this.ClientSize = new System.Drawing.Size(780, 406);
+            this.Controls.Add(this.cmbTheme);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cebView);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtPermissionName);
+            this.Controls.Add(this.label8);
             this.Controls.Add(this.txtDatatName);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.txtPwd);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtCodePath);
             this.Controls.Add(this.btnGenerator);
             this.Controls.Add(this.cmbDb);
             this.Controls.Add(this.label2);
@@ -234,15 +267,18 @@
         private System.Windows.Forms.ComboBox cmbDb;
         private System.Windows.Forms.TextBox txtServerName;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtCodePath;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtUsername;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox txtPwd;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox txtDatatName;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPermissionName;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox cebView;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmbTheme;
     }
 }
 
--- ABPGenerator/Default.cs
+++ ABPGenerator/Default.cs
@@ -22,17 +22,22 @@ namespace ABPBuilder.Tools
 
             InitializeComponent();
 
-            //默认保存在桌面-方便直接复制到项目里
-            string dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-            txtCodePath.Text = dir;
+            ////默认保存在桌面-方便直接复制到项目里
+            //string dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            //txtCodePath.Text = dir;
+
 
             //配置默认的连接数据库字符串
-            txtServerName.Text = @"";
-            txtUsername.Text = "";
-            txtPwd.Text = "";
+            txtServerName.Text = @"192.168.1.223\sqk2k8r2";
+            txtUsername.Text = "sa";
+            txtPwd.Text = "sa2008r2";
+            txtDatatName.Text = "TJ_MedicalAdmissionsDB";
+
+            txtPermissionName.Text = "PermissionNames.Sys";
 
 
-
+            List<string> listTheme = new List<string> { "Default", "Metronic" };
+            cmbTheme.DataSource = listTheme;
         }
 
         private void btnConnection_Click(object sender, EventArgs e)
@@ -52,6 +57,7 @@ namespace ABPBuilder.Tools
                     string conn = string.Format("Server={0}; Database={1}; User ID={2}; Password={3};", txtServerName.Text, txtDatatName.Text, txtUsername.Text, txtPwd.Text);
                     list = ABPGenerator.Common.DapperHelper.GetTable(conn);
                     cmbDb.DataSource = list;
+
                 }
                 catch (Exception ex)
                 {
@@ -64,81 +70,88 @@ namespace ABPBuilder.Tools
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            FolderBrowserDialog dialog = new FolderBrowserDialog();
-            dialog.Description = "请选择文件夹路径";
-
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                string foldPath = dialog.SelectedPath;
-                txtCodePath.Text = foldPath;
-            }
-        }
-
         private void btnGenerator_Click(object sender, EventArgs e)
         {
-            if (txtCodePath.Text == "")
-            {
-                MessageBox.Show("请选择文件夹路径");
-            }
+
             //string conn = string.Format("Server={0}; Database={1}; User ID={2}; Password={3};", txtServerName.Text, txtDatatName.Text, txtUsername.Text, txtPwd.Text);
             //var list = ABPGenerator.Common.DapperHelper.GetField(conn, this.cmbDb.Text);
 
             try
             {
-                if (list != null && list.Count() > 0)
+
+                var _fileName = this.cmbDb.Text.Replace("Tb", "");
+
+
+                CustomTextTemplatingEngineHost host = new CustomTextTemplatingEngineHost();
+                host.Session = new TextTemplatingSession();
+                host.Session.Add("classname", _fileName);
+                host.Session.Add("lowername", _fileName.ToLower());
+                host.Session.Add("permissionName", txtPermissionName.Text.Trim());
+
+                //文件保存地址
+                string dir = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                string _save_path = dir + "/" + _fileName + "s";
+
+                string _dto_path = _save_path + "/Dto";
+                if (Directory.Exists(_dto_path) == false)
                 {
-                    var _fileName = this.cmbDb.Text.Replace("Tb", "");
-
-
-                    CustomTextTemplatingEngineHost host = new CustomTextTemplatingEngineHost();
-                    host.Session = new TextTemplatingSession();
-                    host.Session.Add("classname", _fileName);
-                    host.Session.Add("list", list);
-
-
-                    //文件保存地址
-                    string _save_path = txtCodePath.Text + "/" + _fileName + "s";
-
-                    string _dto_path = _save_path + "/Dto";
-                    if (Directory.Exists(_dto_path) == false)
-                    {
-                        Directory.CreateDirectory(_dto_path);
-                    }
-
-
-                    //保存生成代码后的路径，可以根据实际项目需求修改
-                    Dictionary<string, string> dicPath = new Dictionary<string, string>();
-
-                    dicPath.Add("Interface.tt", "/I" + _fileName + "AppService.cs"); //create input
-                    dicPath.Add("EntityAppService.tt", "/" + _fileName + "AppService.cs"); //create input
-
-
-                    dicPath.Add("CreateInput.tt", "/Dto/Create" + _fileName + "Input.cs"); //create input
-                    dicPath.Add("Dto.tt", "/Dto/" + _fileName + "Dto.cs"); //Dto                                                
-                    dicPath.Add("GetInput.tt", "/Dto/Get" + _fileName + "Input.cs"); //GetInput
-                    dicPath.Add("EditInput.tt", "/Dto/Edit" + _fileName + "Input.cs"); //EditInput
-
-                    foreach (var item in dicPath)
-                    {
-                        #region 
-                        var templatePath = System.Windows.Forms.Application.StartupPath + "/Template/" + item.Key; ;
-                        host.TemplateFileValue = templatePath;
-                        string input = System.IO.File.ReadAllText(templatePath);
-                        string output = new Engine().ProcessTemplate(input, host);
-                        System.IO.File.WriteAllText(_save_path + item.Value, output.TrimStart(), Encoding.UTF8);
-                        #endregion
-                    }
-
-                    MessageBox.Show("代码生成成功");
-                    //if (MessageBox.Show("打开代码所在文件吗？", "生成成功", MessageBoxButtons.OKCancel) == DialogResult.OK)
-
-                    //{
-                    //    System.Diagnostics.Process.Start(_save_path);
-                    //}
-
+                    Directory.CreateDirectory(_dto_path);
                 }
+
+                string _controller_path = _save_path + "/Controller";
+
+
+                string _view_path = _save_path + "/Controller/Views/" + _fileName;
+
+                if (cebView.Checked)
+                {
+                    if (Directory.Exists(_controller_path) == false)
+                    {
+                        Directory.CreateDirectory(_controller_path);
+                    }
+
+                    if (Directory.Exists(_view_path) == false)
+                    {
+                        Directory.CreateDirectory(_view_path);
+                    }
+                }
+
+
+                //保存生成代码后的路径，可以根据实际项目需求修改
+                Dictionary<string, string> dicPath = new Dictionary<string, string>();
+
+                dicPath.Add("/WP.Application/Interface.tt", "/I" + _fileName + "AppService.cs"); //create input
+                dicPath.Add("/WP.Application/EntityAppService.tt", "/" + _fileName + "AppService.cs"); //create input
+
+
+                dicPath.Add("/WP.Application/CreateInput.tt", "/Dto/Create" + _fileName + "Input.cs"); //create input
+                dicPath.Add("/WP.Application/Dto.tt", "/Dto/" + _fileName + "Dto.cs"); //Dto                                                
+                dicPath.Add("/WP.Application/GetInput.tt", "/Dto/Get" + _fileName + "Input.cs"); //GetInput
+                dicPath.Add("/WP.Application/EditInput.tt", "/Dto/Edit" + _fileName + "Input.cs"); //EditInput
+
+                /*Controller*/
+
+                if (cebView.Checked)
+                {
+                    dicPath.Add("/WP.Web/Controller.tt", "/Controller/" + _fileName + "Controller.cs"); //Controller
+
+                    dicPath.Add("/WP.Web/Views/"+cmbTheme.Text+"/Index.tt", "/Controller/Views/" + _fileName + "/Index.cshtml");
+                    dicPath.Add("/WP.Web/Views/" + cmbTheme.Text + "/Create.tt", "/Controller/Views/" + _fileName + "/Create.cshtml");
+                    dicPath.Add("/WP.Web/Views/" + cmbTheme.Text + "/Edit.tt", "/Controller/Views/" + _fileName + "/Edit.cshtml");
+                }
+
+                foreach (var item in dicPath)
+                {
+                    #region 
+                    var templatePath = System.Windows.Forms.Application.StartupPath + "/Template/" + item.Key;
+                    host.TemplateFileValue = templatePath;
+                    string input = System.IO.File.ReadAllText(templatePath);
+                    string output = new Microsoft.VisualStudio.TextTemplating.Engine().ProcessTemplate(input, host);
+                    System.IO.File.WriteAllText(_save_path + item.Value, output.TrimStart(), Encoding.UTF8);
+                    #endregion
+                }
+
+                MessageBox.Show("代码生成成功,已保存到桌面");
             }
             catch (Exception ex)
             {
@@ -146,5 +159,11 @@ namespace ABPBuilder.Tools
             }
 
         }
+
+        private void cmbDb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // txtPermissionName.Text = "namespace WP." + cmbDb.Text.Replace("Tb", "") + "s.Dto";
+        }
+
     }
 }
